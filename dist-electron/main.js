bfd333386d1adb95bb5dfcae034ab8e62a05ee5d
zzzryt <jinjinstar3@gmail.com>
2025-11-17 17:17:03 +0900

    feat(package): 앱 배포 패키징 진행

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,9 +1,9 @@
 import "node:path";
 import "node:url";
 import "electron";
-import { M, R, V } from "./main-_ujEwW9t.js";
+import { M as m, R as p, V as t } from "./main-YD958lz_.js";
 export {
-  M as MAIN_DIST,
-  R as RENDERER_DIST,
-  V as VITE_DEV_SERVER_URL
+  m as MAIN_DIST,
+  p as RENDERER_DIST,
+  t as VITE_DEV_SERVER_URL
 };
---
+++ forge.config.js
@@ -0,0 +1,46 @@
+import { FusesPlugin } from '@electron-forge/plugin-fuses';
+import { FuseV1Options, FuseVersion } from '@electron/fuses';
+
+export const packagerConfig = {
+  asar: true,
+  icon: './src/assets/matmal-icon',
+  osxSign: {},
+};
+export const rebuildConfig = {};
+export const makers = [
+  {
+    name: '@electron-forge/maker-squirrel',
+    config: {
+      name: 'matmal_app',
+    },
+  },
+  {
+    name: '@electron-forge/maker-zip',
+    platforms: ['darwin'],
+  },
+  {
+    name: '@electron-forge/maker-deb',
+    config: {},
+  },
+  {
+    name: '@electron-forge/maker-rpm',
+    config: {},
+  },
+];
+export const plugins = [
+  {
+    name: '@electron-forge/plugin-auto-unpack-natives',
+    config: {},
+  },
+  // Fuses are used to enable/disable various Electron functionality
+  // at package time, before code signing the application
+  new FusesPlugin({
+    version: FuseVersion.V1,
+    [FuseV1Options.RunAsNode]: false,
+    [FuseV1Options.EnableCookieEncryption]: true,
+    [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+    [FuseV1Options.EnableNodeCliInspectArguments]: false,
+    [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+    [FuseV1Options.OnlyLoadAppFromAsar]: true,
+  }),
+];
