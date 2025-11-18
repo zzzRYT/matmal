2464e6ee24ee7f3b8a7ee5958ad5924f40069bcf
zzzryt <jinjinstar3@gmail.com>
2025-11-18 15:46:31 +0900

    feat(relase): 배포 플로우 작성

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,19 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'zzzryt',
+          name: 'matmal',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
