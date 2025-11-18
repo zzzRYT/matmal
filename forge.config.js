bc7d4e915ef9db2300c7b391945e3f981898ed68
zzzryt <jinjinstar3@gmail.com>
2025-11-18 16:33:49 +0900

    fix(publish): excuteableName추가

--- forge.config.js
+++ forge.config.js
@@ -5,6 +5,8 @@ export default {
   packagerConfig: {
     asar: true,
     icons: './src/assets/matmal-icon',
+    executableName: 'MATMAL',
+    name: 'MATMAL',
   },
   rebuildConfig: {},
   makers: [
