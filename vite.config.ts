e8b6fe73a11ccdb2e9d5008e39ea3df10262b84e
zzzryt <jinjinstar3@gmail.com>
2025-11-18 23:22:56 +0900

    fix(publish): wind mac에 대한 flow수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,13 @@ import electron from 'vite-plugin-electron/simple';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [
     tailwindcss(),
     react(),
     electron({
       main: {
-        // Shortcut of `build.lib.entry`.
         entry: 'electron/main.ts',
-        // Ensure optional native deps used by some packages (eg. ws -> bufferutil)
-        // are left external during rollup bundling so Node can resolve them at runtime
-        // or they can be installed separately. This avoids build-time resolution errors.
         vite: {
           build: {
             rollupOptions: {
@@ -26,18 +20,9 @@ export default defineConfig({
         },
       },
       preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
         input: path.join(__dirname, 'electron/preload.ts'),
       },
-      // Ployfill the Electron and Node.js API for Renderer process.
-      // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer:
-        process.env.NODE_ENV === 'test'
-          ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
-            undefined
-          : {},
+      renderer: process.env.NODE_ENV === 'test' ? undefined : {},
     }),
   ],
 });
